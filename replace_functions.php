bc0243751d1eca055dfa69cb3eab4a6882b6e670
unknown <danhoat@gmail.com>
2022-11-29 01:58:37 +0700

    add new contact

--- replace_functions.php
+++ replace_functions.php
@@ -3,6 +3,8 @@
 function cs_mje_show_contact_link($to_user){
 	$user = get_userdata($to_user);
 	?>
-	  <li><a href="" class="contact-link do-contact" data-touser= "<?php echo $user->user_login; ?>"><?php _e('Contact me', 'enginethemes');?><i class="fa fa-comment"></i></a></li><?php
+	  <li><a href="" class="create-chat " data-touser= "<?php echo $user->user_login; ?>"><?php _e('Contact me', 'enginethemes');?><i class="fa fa-comment"></i></a></li>
+	  <div id="message-form"></div>
+	  <?php
 
 }
