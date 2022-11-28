bd9ff7d5db4262e7783fbc37b43f2a8fb7bf7586
unknown <danhoat@gmail.com>
2022-11-29 02:24:03 +0700

    udpate css

--- replace_functions.php
+++ replace_functions.php
@@ -3,7 +3,7 @@
 function cs_mje_show_contact_link($to_user){
 	$user = get_userdata($to_user);
 	?>
-	  <li><span class="create-chat " data-touser= "<?php echo $user->user_login; ?>"><?php _e('Contact me', 'enginethemes');?><i class="fa fa-comment"></i></span></li>
+	  <li><a href="#" class="create-chat contact-link" data-touser= "<?php echo $user->user_login; ?>"><?php _e('Contact me', 'enginethemes');?><i class="fa fa-comment"></i></a></li>
 	  <div id="message-form"></div>
 	  <?php
 
